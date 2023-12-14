e7710a93faaef96a83ba32bf041e134c4730cf6f
Tejas Gupta <131871888+multiverseweb@users.noreply.github.com>
2023-12-15 02:23:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ function hide() {
     document.getElementById("show").style = "display:block";
     document.getElementById("hide").style = "display:none";
 }
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  }
 function changeCss() {
     var bodyElement = document.getElementById("body");
     var cover = document.getElementById("cover");
     var blob = document.getElementById("blob");
+    mybutton = document.getElementById("myBtn");
+    this.scrollY < 1 ? blob.style.top = "-17%" : blob.style.left = "45vw";
+    this.scrollY < 1 ? blob.style.left = "-20%" : blob.style.left = "170px";
     this.scrollY > 30 ? cover.style.opacity = 0 : cover.style.opacity = 1;
+    this.scrollY > 500 ? myBtn.style.display = "block" : myBtn.style.display = "none";
     this.scrollY > 1700 ? blob.style.left = "-100px" : blob.style.left = "170px";
     this.scrollY > 1700 ? blob.style.top = "-35vw" : blob.style.top = "45vw";
 }

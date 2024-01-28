6f34028b424543b1d39afd9ad6f2935998402cea
Tejas Gupta <131871888+multiverseweb@users.noreply.github.com>
2024-01-29 04:12:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,15 +77,3 @@ window.addEventListener("scroll", function () {
     parallax2();
 });
 
-
-$(document).ready(function () {
-
-    if (localStorage.getItem("my_app_name_here-quote-scroll") != null) {
-        $(window).scrollTop(localStorage.getItem("my_app_name_here-quote-scroll"));
-    }
-
-    $(window).on("scroll", function() {
-        localStorage.setItem("my_app_name_here-quote-scroll", $(window).scrollTop());
-    });
-
-  });
